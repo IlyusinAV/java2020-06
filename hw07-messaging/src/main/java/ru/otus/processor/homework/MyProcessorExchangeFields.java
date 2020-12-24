879c06dc59a4d867fba9317680eff61fa9c06f82
ilyusinav <andreyilusin@mail.ru>
2020-12-24 13:17:52 +0300

    2. Сделать процессор, который поменяет местами значения field11 и field12 3. Сделать процессор, который будет выбрасывать исключение в четную секунду (сделайте тест с гарантированным результатом) 4. Сделать Listener для ведения истории: старое сообщение - новое

--- hw07-messaging/src/main/java/ru/otus/processor/homework/MyProcessorExchangeFields.java
+++ hw07-messaging/src/main/java/ru/otus/processor/homework/MyProcessorExchangeFields.java
@@ -1,11 +1,12 @@
 package ru.otus.processor.homework;
 
 import ru.otus.MyMessage;
+import ru.otus.processor.homework.exceptions.EvenSecondException;
 
 public class MyProcessorExchangeFields implements MyProcessor {
 
     @Override
-    public MyMessage process(MyMessage message) {
+    public MyMessage process(MyMessage message) throws EvenSecondException {
         var newFieldValue = message.getField11();
         return message.toBuilder().field11(message.getField12()).field12(newFieldValue).build();
     }
