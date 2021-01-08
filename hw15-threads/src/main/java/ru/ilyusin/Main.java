efb05fe0517929d8c0453529ca4f5dc8260abdd4
ilyusinav <andreyilusin@mail.ru>
2021-01-08 17:09:42 +0300

    Initial commit.

--- hw15-threads/src/main/java/ru/ilyusin/Main.java
+++ hw15-threads/src/main/java/ru/ilyusin/Main.java
@@ -2,21 +2,27 @@ package ru.ilyusin;
 
 import ru.ilyusin.utils.OscillatorImpl;
 
-import java.time.LocalTime;
-
 class Main {
-    public static void main(String... args) throws InterruptedException {
+    public static void main(String... args) {
         var oscillator = new OscillatorImpl();
 
         JThread t1 = new JThread("Thread1", oscillator);
         JThread t2 = new JThread("Thread2", oscillator);
 
-        System.out.println("Starting: " + LocalTime.now());
         t1.start();
-        t1.join();
+
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         t2.start();
-        t2.join();
-        System.out.println("Finished: " + LocalTime.now());
+
+        try {
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
