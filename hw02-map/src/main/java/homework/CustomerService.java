0e6013ce87ce596e3652005f59c8f2bac3654fe4
ilyusinav <andreyilusin@mail.ru>
2021-01-20 14:21:44 +0300

    Первые 3 теста проходят.

--- hw02-map/src/main/java/homework/CustomerService.java
+++ hw02-map/src/main/java/homework/CustomerService.java
@@ -2,22 +2,32 @@ package homework;
 
 
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CustomerService {
+    private final TreeMap<Customer, String> customerService = new TreeMap<>();
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        TreeMap<Customer, String> smallest = new TreeMap<>();
+        var customer = customerService.firstEntry().getKey();
+        var newCustomer = new Customer(customer.getId(), customer.getName(), customer.getScores());
+        smallest.put(newCustomer, customerService.firstEntry().getValue());
+        return smallest.firstEntry();
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        TreeMap<Customer, String> nextEntry = new TreeMap<>();
+        var nextValue = customerService.higherEntry(customer).getValue();
+        nextEntry.put(customer, nextValue);
+        return nextEntry.firstEntry();
     }
 
     public void add(Customer customer, String data) {
-
+        var newCustomer = new Customer(customer.getId(), customer.getName(), customer.getScores());
+        customerService.put(newCustomer, data);
     }
 }
