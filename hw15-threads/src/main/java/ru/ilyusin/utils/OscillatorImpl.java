968c9c2965189b939785f402ebb0337bb03bdd18
ilyusinav <andreyilusin@mail.ru>
2021-01-25 12:59:42 +0300

    Solving Git problems.

--- hw15-threads/src/main/java/ru/ilyusin/utils/OscillatorImpl.java
+++ hw15-threads/src/main/java/ru/ilyusin/utils/OscillatorImpl.java
@@ -1,8 +1,8 @@
 package ru.ilyusin.utils;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Arrays;
+import java.util.concurrent.*;
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 import java.util.List;
 import java.util.ArrayList;
@@ -10,43 +10,9 @@ import java.util.ArrayList;
 public class OscillatorImpl implements Oscillator {
     private static final int WAVELENGTH = 18;
     private static final int PERIODS = 2;
-    private List<Integer> items;
-    private CompletableFuture[] jobs = new CompletableFuture[WAVELENGTH];
+    private final List<Integer> oscillation = new ArrayList<>();
 
     public OscillatorImpl() {
-        items = this.build();
-    }
-
-    public void run() throws ExecutionException, InterruptedException {
-        synchronized (items) {
-            int completed;
-
-            for (int times = 0; times < PERIODS; times++) {
-                completed = 0;
-                for (int i = 0; i < WAVELENGTH; i++) {
-                    int finalI = i;
-                    CompletableFuture job = CompletableFuture.supplyAsync(() -> getItem(finalI));
-                    jobs[i] = job;
-                }
-                while (completed < WAVELENGTH) {
-                    for (int j = 0; j < WAVELENGTH; j++) {
-                        if (jobs[j].isDone()) {
-                            completed++;
-                        }
-                    }
-                }
-                for (int k = 0; k < WAVELENGTH; k++) {
-                    jobs[k].thenAccept(result -> System.out.print(result + ", "));
-                }
-            }
-
-            System.out.print("\n");
-        }
-    }
-
-    private List<Integer> build() {
-        List<Integer> oscillation = new ArrayList<>();
-
         IntStream natural = IntStream.iterate(1, i -> i + 1);
         natural
                 .limit(10)
@@ -56,17 +22,47 @@ public class OscillatorImpl implements Oscillator {
         descending
                 .limit(8)
                 .forEach(oscillation::add);
+    }
 
-        return oscillation;
+    public void run() {
+        var forkJoinPool = new ForkJoinPool();
+        ForkJoinTask<String> futureTask = new RecursiveTask<String>() {
+            @Override
+            protected String compute() {
+                return outWave();
+            }
+        };
+        for (int i = 0; i < PERIODS; i++) {
+            forkJoinPool.submit(futureTask);
+            System.out.print(futureTask.join());
+        }
+        System.out.print("\n");
+    }
+
+    private String outWave() {
+        StringBuilder wave = new StringBuilder();
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        CompletableFuture[] tasks = new CompletableFuture[WAVELENGTH];
+        IntStream.range(0, WAVELENGTH).forEach(i -> {
+            int finalI = i;
+            Supplier<String> task = () -> getItem(finalI);
+            tasks[i] = CompletableFuture.supplyAsync(task, service);
+        });
+
+        CompletableFuture<Void> allTasks = CompletableFuture.allOf(tasks);
+        allTasks.thenRun(() -> {
+            Arrays.stream(tasks).forEach(result -> wave.append(result.join()));
+        });
+        service.shutdown();
+        return wave.toString();
     }
 
     private String getItem(int i) {
         try {
-            Thread.sleep(1000);
+            TimeUnit.SECONDS.sleep(1);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return items.get(i).toString();
+        return oscillation.get(i).toString() + ", ";
     }
-
 }
