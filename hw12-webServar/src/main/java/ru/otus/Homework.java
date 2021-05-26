16294cd9393b48493483b3aefb3056dd844a15a7
ilyusinav <andreyilusin@mail.ru>
2021-05-26 11:36:39 +0300

    hw12-webServer. Init functions for DB and Web Server added.

--- hw12-webServar/src/main/java/ru/otus/Homework.java
+++ hw12-webServar/src/main/java/ru/otus/Homework.java
@@ -1,15 +1,26 @@
 package ru.otus;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.hibernate.cfg.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.core.repository.DataTemplateHibernate;
 import ru.otus.core.repository.HibernateUtils;
 import ru.otus.core.sessionmanager.TransactionManagerHibernate;
+import ru.otus.crm.dbmigrations.MigrationsExecutorFlyway;
 import ru.otus.crm.model.AddressDataSet;
 import ru.otus.crm.model.Client;
 import ru.otus.crm.model.PhoneDataSet;
 import ru.otus.crm.service.DbServiceClientImpl;
+import ru.otus.dao.InMemoryUserDao;
+import ru.otus.dao.UserDao;
+import ru.otus.server.UsersWebServer;
+import ru.otus.server.UsersWebServerWithFilterBasedSecurity;
+import ru.otus.services.TemplateProcessor;
+import ru.otus.services.TemplateProcessorImpl;
+import ru.otus.services.UserAuthService;
+import ru.otus.services.UserAuthServiceImpl;
 
 import java.util.List;
 
@@ -17,9 +28,25 @@ public class Homework {
     private static final Logger log = LoggerFactory.getLogger(Homework.class);
 
     public static final String HIBERNATE_CFG_FILE = "hibernate.cfg.xml";
+    private static final int WEB_SERVER_PORT = 8080;
+    private static final String TEMPLATES_DIR = "/templates/";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        var dbServiceClient = initDB();
+
+        var usersWebServer = initWebServer();
+        usersWebServer.start();
+        usersWebServer.join();
+    }
+
+    private static DbServiceClientImpl initDB() {
         var configuration = new Configuration().configure(HIBERNATE_CFG_FILE);
+
+        var dbUrl = configuration.getProperty("hibernate.connection.url");
+        var dbUserName = configuration.getProperty("hibernate.connection.username");
+        var dbPassword = configuration.getProperty("hibernate.connection.password");
+
+        new MigrationsExecutorFlyway(dbUrl, dbUserName, dbPassword).executeMigrations();
 
         var sessionFactory = HibernateUtils.buildSessionFactory(configuration, Client.class, AddressDataSet.class, PhoneDataSet.class);
         var transactionManager = new TransactionManagerHibernate(sessionFactory);
@@ -27,11 +54,18 @@ public class Homework {
         var clientTemplate = new DataTemplateHibernate<>(Client.class);
         var dbServiceClient = new DbServiceClientImpl(transactionManager, clientTemplate);
 
-        var client1 = new Client("dbServiceFirst", new AddressDataSet("First street"));
+        return dbServiceClient;
+    }
 
-        var phones = List.of(new PhoneDataSet("1111111111", client1));
-        client1.setPhones(phones);
+    private static UsersWebServer initWebServer() throws Exception {
+        UserDao userDao = new InMemoryUserDao();
+        Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+        TemplateProcessor templateProcessor = new TemplateProcessorImpl(TEMPLATES_DIR);
+        UserAuthService authService = new UserAuthServiceImpl(userDao);
 
-        var clientSaved = dbServiceClient.saveClient(client1);
+        UsersWebServer usersWebServer = new UsersWebServerWithFilterBasedSecurity(WEB_SERVER_PORT,
+                authService, userDao, gson, templateProcessor);
+
+        return usersWebServer;
     }
 }
